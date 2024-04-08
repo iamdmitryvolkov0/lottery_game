d855f56beebbf7c62a83acefc24c7d8c181688a7
DmitryVolkov <iamdmitryvolkov@gmail.com>
2024-04-08 15:48:12 +0700

    Validation

--- project/app/Actions/LotteryGameActions/CreateLotteryGameMatchUsersAction.php
+++ project/app/Actions/LotteryGameActions/CreateLotteryGameMatchUsersAction.php
@@ -3,11 +3,22 @@
 namespace App\Actions\LotteryGameActions;
 
 use App\Models\LotteryGameMatchUser;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Validator;
 
 class CreateLotteryGameMatchUsersAction
 {
-    public function execute(array $data): LotteryGameMatchUser
+    public function execute(array $data): JsonResponse|LotteryGameMatchUser
     {
+
+        $validator = Validator::make($data, [
+            'user_id' => ['required', 'integer'],
+            'lottery_game_match_id' => ['required', 'integer'],
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
         return LotteryGameMatchUser::create([
             'user_id' => $data['user_id'],
             'lottery_game_match_id' => $data['lottery_game_match_id'],
